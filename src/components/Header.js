c52a4eac0a8e7c3fa8ef63c8ee5ccd3984f45b1e
luckyJessie <951034032@qq.com>
2018-10-22 16:19:42 +0800

    edit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+/**
+ * Created by caofan on 2018/10/22.
+ */
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Head</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+/**
+ * Created by caofan on 2018/10/22.
+ */
+/**
+ * Created by caofan on 2018/10/22.
+ */
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Home</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
